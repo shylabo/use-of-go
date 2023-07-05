387c66c69d2b40b3e23564ba4394dec83cf7acaf
shylabo <shuya.sohot@gmail.com>
2023-07-05 13:37:01 -0700

    update: 01_hello-world

--- 01_hello-world/main.go
+++ 01_hello-world/main.go
@@ -3,14 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World") // Hello World
 }
-
-/**
-Run without Build
-$ go run main.go
-
-Build & Run
-$ go build
-$ ./hello-world
-*/
