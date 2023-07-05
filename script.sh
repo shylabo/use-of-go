44aea1f9cbbddef1ef146033ed6ae251feb2d3f2
shylabo <shuya.sohot@gmail.com>
2023-07-05 13:22:07 -0700

    add: scripts for automatic generation

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read -p "Enter directory name: " dir_name
+
+mkdir "$dir_name"
+
+cd "$dir_name"
+
+cat <<EOF > main.go
+package main
+
+func main() {
+  // Add code
+}
+EOF
+
+go mod init "github.com/shylabo/use-of-go/$dir_name"
+
+cd ..
+
+go work use "./$dir_name"
