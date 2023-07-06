061e10195614c0b16fb38632281e28080b7696f3
shylabo <shuya.sohot@gmail.com>
2023-07-06 11:02:00 -0700

    add: 19_struct

---
+++ 19_struct/go.mod
@@ -0,0 +1,3 @@
+module github.com/shylabo/use-of-go/19_struct
+
+go 1.18
---
+++ 19_struct/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func newPerson(name string) *Person {
+	p := Person{name: name}
+	p.age = 29
+
+	return &p
+}
+
+func main() {
+	fmt.Println(Person{"Bob", 20})
+	fmt.Println(Person{name: "Alice", age: 30})
+	fmt.Println(Person{name: "Fred"})
+	fmt.Println(&Person{name: "Ann", age: 40})
+	fmt.Println(newPerson("John"))
+
+	s := Person{name: "Sean", age: 50}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
+
+	dog := struct {
+		name   string
+		isGood bool
+	}{
+		"Rex",
+		true,
+	}
+	fmt.Println(dog)
+}
